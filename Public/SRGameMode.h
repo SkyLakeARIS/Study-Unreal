42ddebdf47238a4c0dd17e28e3a9614abb874399
SkyLake <bunkete15@gmail.com>
2022-08-26 00:09:01 +0900

    코드 최적화
    GameMode 클래스를 하나로 통합하고 블루프린트 클래스로 게임 모드 타입을 지정하도록하여 코드 관점에서 개선함.

--- Public/SRGameMode.h
+++ Public/SRGameMode.h
@@ -5,28 +5,34 @@
 #include "Version.h"
 #include "GameModeData.h"
 #include "GameFramework/GameModeBase.h"
-#include "SRLongRangeGameMode.generated.h"
+#include "SRGameMode.generated.h"
 
-/*
- * LongRange 고정형 맵을 선택할 때의 게임모드입니다.
+/**
+ *  맵을 선택할 때의 게임모드입니다.
+ *	블루프린트로 상속하여 구현합니다.
  */
-UCLASS()
-class VERSION_API ASRLongRangeGameMode : public AGameModeBase
+UCLASS(BlueprintType, Blueprintable)
+class VERSION_API ASRGameMode : public AGameModeBase
 {
 	GENERATED_BODY()
+
 public:
-	ASRLongRangeGameMode();
+
+	ASRGameMode();
 
 	virtual void PostInitializeComponents() override;
 
 	virtual void PostLogin(APlayerController* NewPlayer) override;
 
+
 private:
 
 	void initTargetManagerMode();
 
-private:
+protected:
+	UPROPERTY(BlueprintReadWrite)
 	EGameModeType mGameModeType;
+
 	UPROPERTY()
 	class ASRPlayerController* playerController;
 };
