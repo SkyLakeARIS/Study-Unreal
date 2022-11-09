1bb71f27c631f074980a27af2e3a250c56678eb6
SkyLake <bunkete15@gmail.com>
2022-11-09 21:19:00 +0900

    불필요한 변수 및 함수 제거
    불필요한 변수 및 함수 제거

--- Private/SRProjectile.cpp
+++ Private/SRProjectile.cpp
@@ -3,10 +3,11 @@
 #include "SRPlayerController.h"
 #include "DrawDebugHelpers.h"
 #include "SRTargetCharacter.h"
-#include "SRPlayerCharacter.h"
 #include "UIHUDWidget.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Components/SphereComponent.h"
+
+bool ASRProjectile::bIsDebugMode = false;
 
 ASRProjectile::ASRProjectile() 
 {
@@ -32,14 +33,12 @@ ASRProjectile::ASRProjectile()
 
 	InitialLifeSpan = 10.0f;
 	mbIsCollision = false;
-	mbIsTargetHit = false;
-	mbDebugMode = false;
 }
 
 void ASRProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	if (mbIsCollision == false && mbDebugMode)
+	if (mbIsCollision == false && bIsDebugMode)
 	{
 		DrawDebugPoint(GetWorld(), GetActorLocation(), 5.0f, FColor::Green, false, 15.0f);
 	}
@@ -61,16 +60,6 @@ void ASRProjectile::EndPlay(const EEndPlayReason::Type EndPlayReason)
 	mShowHitmark.Unbind();
 }
 
-USphereComponent* ASRProjectile::GetCollisionComp() const
-{
-	return mCollisionComp; 
-}
-
-UProjectileMovementComponent* ASRProjectile::GetProjectileMovement() const
-{
-	return mProjectileMovement;
-}
-
 void ASRProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
 	mbIsCollision = true;
@@ -84,41 +73,48 @@ void ASRProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPri
 		return;
 	}
 
-	mHitType = EHitType::Hit;
+	EHitType hitType = EHitType::Hit;
 	const float distanceToHitPoint = (GetActorLocation() - mStartLocation).Size();
 
 	// 총알 타입에 맞는 데미지 테이블을 참고하여 거리에 따른 최종 데미지를 계산합니다.
 	// 1이 cm라는 가정하에 50미터마다 데미지가 1씩 감소합니다. 데미지 감소는 최솟값 미만이 되지 않습니다.
+	int32 bulletDamage = 0;
 	const FWeaponDamage damageTable;
 	switch(mBulletType)
 	{
 		case eWeaponType::AR:
-			mBulletDamage = FMath::Clamp(damageTable.AR - static_cast<int32>(distanceToHitPoint / 5000.0f), damageTable.AR_Min, damageTable.AR);
+			bulletDamage = FMath::Clamp(damageTable.AR - static_cast<int32>(distanceToHitPoint / 5000.0f), damageTable.AR_Min, damageTable.AR);
 			break;
 		case eWeaponType::SR:
-			mBulletDamage = FMath::Clamp(damageTable.SR - static_cast<int32>(distanceToHitPoint / 5000.0f), damageTable.SR_Min, damageTable.SR);
+			bulletDamage = FMath::Clamp(damageTable.SR - static_cast<int32>(distanceToHitPoint / 5000.0f), damageTable.SR_Min, damageTable.SR);
 			break;
 		case eWeaponType::HG:
-			mBulletDamage = FMath::Clamp(damageTable.HG - static_cast<int32>(distanceToHitPoint / 5000.0f), damageTable.HG_Min, damageTable.HG);
+			bulletDamage = FMath::Clamp(damageTable.HG - static_cast<int32>(distanceToHitPoint / 5000.0f), damageTable.HG_Min, damageTable.HG);
 			break;
 		default:
 			checkf(false, TEXT("ASRProjectile - OnHit - eWeaponType 올바르지 않은 enum 타입입니다."));
 			break;
 	}
 
+	// 헤드샷 여부를 검사합니다.
+	bool bIsHeadshot = false;
 	if (Hit.GetComponent()->GetName().Contains(FString("Head")))
 	{
-		mBulletDamage *= 1.5f;
-		mbIsHeadshot = true;
-		mHitType = EHitType::HeadShot;
+		bulletDamage *= 1.5f;
+		bIsHeadshot = true;
+		hitType = EHitType::HeadShot;
 		mOnUpdateHeadshotCount.Execute();
 	}
 
+	/*
+	 *  데미지 계산 후 맞은 타겟에게 적용합니다.
+	 *	그리고 점수를 받아옵니다.
+	 */
 	int32 getScore = 0;
-	const auto targetCharacter = Cast<ASRTargetCharacter>(Hit.GetActor());
-	const bool bIsKill = targetCharacter->OnHit(mBulletDamage, getScore);
+	auto* const targetCharacter = Cast<ASRTargetCharacter>(Hit.GetActor());
+	const bool bIsKill = targetCharacter->OnHit(bulletDamage, getScore);
 
-	getScore = mbIsHeadshot ? getScore * 1.5f : getScore;
+	getScore = bIsHeadshot ? getScore * 1.5f : getScore;
 	mOnHitAndUpdateAcc.Execute();
 
 	if(bIsKill)
@@ -128,11 +124,12 @@ void ASRProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPri
 		{
 			getScore += static_cast<int32>(distanceToHitPoint);
 		}
-		mHitType = EHitType::Kill;
+		hitType = EHitType::Kill;
 		mOnUpdateKill.Execute();
 	}
+
 	mOnUpdateScore.Execute(getScore);
-	mShowHitmark.Execute(mHitType);
+	mShowHitmark.Execute(hitType);
 	
 	Destroy();
 }
@@ -141,37 +138,32 @@ void ASRProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPri
  * playerstate와 정보를 바인드합니다.(delegate)
  * 대상 정보: Hit count, accuracy, score, kill count
  */
-void ASRProjectile::BindPlayerStateInfo(ASRPlayerState* srPlayerState)
+void ASRProjectile::BindPlayerStateInfo(ASRPlayerState& srPlayerState)
 {
-	mOnHitAndUpdateAcc.BindUObject(srPlayerState, &ASRPlayerState::OnAddHitCount);
-	mOnUpdateScore.BindUObject(srPlayerState, &ASRPlayerState::OnAddScore);
-	mOnUpdateKill.BindUObject(srPlayerState, &ASRPlayerState::OnAddKill);
-	mOnUpdateHeadshotCount.BindUObject(srPlayerState, &ASRPlayerState::OnAddHeadshotCount);
+	mOnHitAndUpdateAcc.BindUObject(&srPlayerState, &ASRPlayerState::OnAddHitCount);
+	mOnUpdateScore.BindUObject(&srPlayerState, &ASRPlayerState::OnAddScore);
+	mOnUpdateKill.BindUObject(&srPlayerState, &ASRPlayerState::OnAddKill);
+	mOnUpdateHeadshotCount.BindUObject(&srPlayerState, &ASRPlayerState::OnAddHeadshotCount);
 }
 
 /*
  * player controller와 정보를 바인드합니다.(delegate)
  * 대상 정보: HUDWidget
  */
-void ASRProjectile::BindHUDWidget(UUIHUDWidget* hud)
+void ASRProjectile::BindHUDWidget(UUIHUDWidget& hud)
 {
-	mShowHitmark.BindUObject(hud, &UUIHUDWidget::AddHitMarkToViewPort);
+	mShowHitmark.BindUObject(&hud, &UUIHUDWidget::AddHitMarkToViewPort);
 }
 
 /*
  * 발사된 총알의 타입을 무기타입으로부터 정하는 함수입니다.
  */
-void ASRProjectile::SetBulletType(eWeaponType gunType)
+void ASRProjectile::SetBulletType(const eWeaponType gunType)
 {
 	mBulletType = gunType;
 }
 
-void ASRProjectile::SetStartLocation(FVector location)
+void ASRProjectile::SetStartLocation(const FVector startLocation)
 {
-	mStartLocation = location;
-}
-
-void ASRProjectile::SetDebugMode(bool active)
-{
-	mbDebugMode = active;
+	mStartLocation = startLocation;
 }
--- Public/SRProjectile.h
+++ Public/SRProjectile.h
@@ -42,33 +42,26 @@ public:
 
 	ASRProjectile();
 
-	virtual void Tick(float DeltaTime) override;
-
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
-	UFUNCTION()
-	void BindPlayerStateInfo(ASRPlayerState* playerState);
+	void BindPlayerStateInfo(ASRPlayerState& playerState);
+
+	void BindHUDWidget(UUIHUDWidget& hud);
 
 	UFUNCTION()
-	void BindHUDWidget(UUIHUDWidget* hud);
-
-	UFUNCTION()
-	void SetBulletType(eWeaponType gunType);
-
-	void SetStartLocation(FVector location);
-
-	void SetDebugMode(bool active);
+	void SetBulletType(const eWeaponType gunType);
+	void SetStartLocation(const FVector location);
 
 protected:
 
 	virtual void BeginPlay() override;
-
+	virtual void Tick(float DeltaTime) override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 
-	USphereComponent* GetCollisionComp() const;
+public:
 
-	UProjectileMovementComponent* GetProjectileMovement() const;
+	static bool bIsDebugMode;
 
 protected:
 	FOnUpdateHitCount mOnHitAndUpdateAcc;
@@ -87,7 +80,6 @@ protected:
 
 private:
 
-	bool mbDebugMode;
 	// UI
 	FOnHitmark mShowHitmark;
 
@@ -96,12 +88,10 @@ private:
 	FOnUpdateScore mOnUpdateScore;
 	FOnUpdateKill mOnUpdateKill;
 	FOnUpdateHeatshotCount mOnUpdateHeadshotCount;
+
+
 	eWeaponType mBulletType;
-	EHitType mHitType;
-	int32 mBulletDamage;
 	bool mbIsCollision;
-	bool mbIsHeadshot;
-	bool mbIsTargetHit;
 };
 
 
